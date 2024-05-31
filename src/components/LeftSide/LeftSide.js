6e6d8e14adfa72e670c6b666201b84389c875b1a
salomon <mtzelisalomon@gmail.com>
2024-05-30 19:08:06 -0600

    connection

--- src/components/LeftSide/LeftSide.js
+++ src/components/LeftSide/LeftSide.js
@@ -8,22 +8,29 @@ import {
   Button,
   TextField,
   Chip,
+
 } from "@mui/material";
 
 function LeftSide() {
   const [selectedTab, setSelectedTab] = useState(0);
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const [results, setResults] = useState({
+    details: {},
+    tag: "",
+    confidenceScores: {},
+  });
 
   const handleChange = (event, newValue) => {
     setSelectedTab(newValue);
   };
 
   const toggleDrawer = (open) => (event) => {
-    // Check if the event is defined and prevent the drawer toggle on certain conditions
-    if (event && event.type === "keydown") {
-      if (event.key === "Tab" || event.key === "Shift") {
-        return;
-      }
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
     }
     setDrawerOpen(open);
   };
@@ -52,9 +59,9 @@ function LeftSide() {
         />
       </Tabs>
       {selectedTab === 0 ? (
-        <SignInForm toggleDrawer={toggleDrawer} />
+        <SignInForm toggleDrawer={toggleDrawer} setResults={setResults} />
       ) : (
-        <SignUpForm toggleDrawer={toggleDrawer} />
+        <SignUpForm toggleDrawer={toggleDrawer} setResults={setResults} />
       )}
       <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer(false)}>
         <Box
@@ -63,67 +70,63 @@ function LeftSide() {
           onClick={toggleDrawer(false)}
           onKeyDown={toggleDrawer(false)}
         >
-          <Box sx={{ justifyContent: "space-between", padding: 3 }}>
-            <Typography
-              variant="h5"
-              color={"secondary"}
-              sx={{ fontWeight: "bold", mb: 2 }}
-            >
-              Random Forest
-            </Typography>
-            <Typography sx={{ mb: 1 }}>Accurancy: 0.5</Typography>
-            <Chip
-              label="Spam"
-              sx={{
-                backgroundColor: "red",
-                color: "white",
-                fontWeight: "bold",
-              }}
-            />
-          </Box>
-          <Box sx={{ justifyContent: "space-between", padding: 3 }}>
-            <Typography
-              variant="h5"
-              color={"secondary"}
-              sx={{ fontWeight: "bold", mb: 2 }}
-            >
-              SVM
-            </Typography>
-            <Typography sx={{ mb: 1 }}>Accurancy: 0.5</Typography>
-            <Chip
-              label="Spam"
-              sx={{
-                backgroundColor: "red",
-                color: "white",
-                fontWeight: "bold",
-              }}
-            />
-          </Box>
-          <Box sx={{ justifyContent: "space-between", padding: 3 }}>
-            <Typography
-              variant="h5"
-              color={"secondary"}
-              sx={{ fontWeight: "bold", mb: 2 }}
-            >
-              Naive Bayes
-            </Typography>
-            <Typography sx={{ mb: 1 }}>Accurancy: 0.5</Typography>
-            <Chip
-              label="Spam"
-              sx={{
-                backgroundColor: "red",
-                color: "white",
-                fontWeight: "bold",
-              }}
-            />
-          </Box>
+          <Typography
+            variant="h5"
+            color="secondary"
+            sx={{ fontWeight: "bold", mb: 2 }}
+          >
+            Result Display
+          </Typography>
+          {results &&
+            results.confidenceScores &&
+            Object.entries(results.confidenceScores).map(([model, score]) => (
+              <Typography key={model} sx={{ mb: 1 }}>
+                {model.toUpperCase()}:{" "}
+                {score ? `${(score * 100).toFixed(2)}%` : "N/A"}
+              </Typography>
+            ))}
+          <Chip
+            label={results.tag}
+            sx={{
+              backgroundColor: results.tag === "Spam" ? "red" : "green",
+              color: "white",
+              fontWeight: "bold",
+              mt: 2,
+            }}
+          />
         </Box>
       </Drawer>
     </Box>
   );
 }
 
-function SignInForm({ toggleDrawer }) {
+function SignInForm({ toggleDrawer, setResults }) {
+  const [formData, setFormData] = useState({
+    emailFrom: "",
+    emailSubject: "",
+    attachmentCount: "",
+    attachmentExtension: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const emailData = `From: ${formData.emailFrom}\nSubject: ${formData.emailSubject}\nBody: Please find the attached file(s). Total attachments: ${formData.attachmentCount}, types: ${formData.attachmentExtension}`;
+
+    const response = await fetch("http://127.0.0.1:5000/predict", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ emailData: emailData }),
+    });
+    const result = await response.json();
+    setResults(result); // Store the result in state
+    toggleDrawer(true)();
+  };
+
   return (
     <Box
       component="form"
@@ -137,28 +140,32 @@ function SignInForm({ toggleDrawer }) {
       }}
       noValidate
       autoComplete="off"
-      onSubmit={(event) => {
-        event.preventDefault();
-        toggleDrawer(true)();
-      }}
+      onSubmit={handleSubmit}
     >
       <TextField
+        name="emailFrom"
         required
         label="Email From"
         variant="outlined"
         fullWidth
         margin="normal"
         placeholder="John Doe <john.doe@example.com>"
+        value={formData.emailFrom}
+        onChange={handleInputChange}
       />
       <TextField
+        name="emailSubject"
         required
         label="Email Subject"
         variant="outlined"
         fullWidth
         margin="normal"
         placeholder="Important: Project Update - Q3 Results"
+        value={formData.emailSubject}
+        onChange={handleInputChange}
       />
       <TextField
+        name="attachmentCount"
         required
         label="Attachment Count"
         variant="outlined"
@@ -166,24 +173,25 @@ function SignInForm({ toggleDrawer }) {
         margin="normal"
         placeholder="3"
         type="number"
+        value={formData.attachmentCount}
+        onChange={handleInputChange}
       />
       <TextField
+        name="attachmentExtension"
         required
         label="Attachment Extension"
         variant="outlined"
         fullWidth
         margin="normal"
         placeholder="pdf, png, xlsx"
+        value={formData.attachmentExtension}
+        onChange={handleInputChange}
       />
       <Button
         type="submit"
         variant="contained"
         color="primary"
         sx={{ mt: 2, width: "100%", fontWeight: "bold" }}
-        onSubmit={(event) => {
-          event.preventDefault();
-          toggleDrawer(true)(event); // Pass the event here
-        }}
       >
         Execute
       </Button>
@@ -191,19 +199,29 @@ function SignInForm({ toggleDrawer }) {
   );
 }
 
-function SignUpForm({ toggleDrawer }) {
+function SignUpForm({ toggleDrawer, setResults }) {
   const [file, setFile] = useState(null);
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     if (!file) {
       alert("Please upload a file.");
       return;
     }
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const response = await fetch("http://127.0.0.1:5000/predict_csv", {
+      method: "POST",
+      body: formData,
+    });
+    const result = await response.json();
+    console.log(result);
+    setResults(result); // Assuming result.results is an array of results
     toggleDrawer(true)();
   };
 
@@ -232,14 +250,6 @@ function SignUpForm({ toggleDrawer }) {
         variant="contained"
         color="primary"
         sx={{ mt: 2, width: "100%", fontWeight: "bold" }}
-        onSubmit={(event) => {
-          event.preventDefault();
-          if (!file) {
-            alert("Please upload a file.");
-            return;
-          }
-          toggleDrawer(true)(event); // Pass the event here
-        }}
       >
         Execute
       </Button>
