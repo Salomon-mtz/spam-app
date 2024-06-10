f67025f126a2adb14ec295d0f5d59cc3eff88590
salomon <mtzelisalomon@gmail.com>
2024-06-10 16:09:35 -0600

    url update

--- src/components/LeftSide/LeftSide.js
+++ src/components/LeftSide/LeftSide.js
@@ -165,7 +165,7 @@ function SignInForm({ toggleDrawer, setResults }) {
     };
 
     const response = await fetch(
-      "https://5027-172-203-93-51.ngrok-free.app/single",
+      "https://184f-172-203-93-51.ngrok-free.app/single",
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -267,7 +267,7 @@ function SignUpForm({ toggleDrawer, setResults, results }) {
     formData.append("file", file);
 
     const response = await fetch(
-      "https://5027-172-203-93-51.ngrok-free.app/csv",
+      "https://184f-172-203-93-51.ngrok-free.app/csv",
       {
         method: "POST",
         body: formData,
